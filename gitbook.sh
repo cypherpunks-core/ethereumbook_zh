9f9b7865a4e38ddf227724d16ef701deff751b91
ChenPoWei <poweichen@gate.sinica.edu.tw>
2019-08-27 03:57:41 +0800

    網頁刷新

--- gitbook.sh
+++ gitbook.sh
@@ -3,7 +3,12 @@ gitbook build
 git add .
 git commit -m $1
 git push -u origin master
-git checkout gh-pages
+git branch -D gh-pages
+git branch  -r -d  origin/gh-pages
+git push origin :gh-pages
+git checkout --orphan gh-pages
+git rm --cached -r .
+git clean -df
 cp -r _book/* .
 git add .
 git commit -m $1
